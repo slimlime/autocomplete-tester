88f4afdc4fbce5251c08b25138af5bd126d31b75
S <SL@SL>
2020-03-21 06:25:23 +1000

    :tada: Init SharedModule

---
+++ src/app/shared/modules/shared/shared.module.ts
@@ -0,0 +1,14 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    IonicModule
+  ]
+})
+export class SharedModule { }
