6613d71ae966a68d30605e4dfd7114f5c0a3f4b3
S <30815731+slimlime@users.noreply.github.com>
2020-03-22 07:13:26 +1000

    :fire: :construction: Refactoring to generics

--- src/app/feature/autocomplete/components/autocomplete/autocomplete.component.ts
+++ src/app/feature/autocomplete/components/autocomplete/autocomplete.component.ts
@@ -1,4 +1,16 @@
-import { Component, OnInit } from "@angular/core";
+import {
+  Component,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChange,
+  SimpleChanges
+} from "@angular/core";
+import { FormControl } from "@angular/forms";
+import { Observable } from "rxjs";
+
+import { AutocompleteSearchFilterable } from "../../models/autocomplete-search-filterable";
+import { AutocompleteService } from "./../../services/autocomplete.service";
 
 /**
  * Component
@@ -6,13 +18,79 @@ import { Component, OnInit } from "@angular/core";
 @Component({
   selector: "app-autocomplete",
   templateUrl: "./autocomplete.component.html",
-  styleUrls: ["./autocomplete.component.scss"],
+  styleUrls: ["./autocomplete.component.scss"]
 })
-export class AutocompleteComponent implements OnInit {
+export class AutocompleteComponent implements OnInit, OnChanges {
+  /**
+   * Nested patient form control of autocomplete patient component
+   * Instantiate due to strict checks. TypeScript setting. strictPropertyInitialization
+   */
+  nestedPatientFormControl: FormControl = new FormControl();
 
+  /**
+   * Input  of autocomplete patient component
+   */
+  // tslint:disable-next-line: no-unsafe-any
+  @Input()
+  set passFormControl(formControlVal: FormControl) {
+    console.log(
+      "AutocompletePatientComponent -> setPassFormControl -> val",
+      formControlVal
+    );
+    this.nestedPatientFormControl = formControlVal;
+  }
+
+  get passFormControl(): FormControl {
+    return this.nestedPatientFormControl;
+  }
+  /**
+   * Creates an instance of autocomplete patient component.
+   */
+  constructor(
+    public autocompletePatientService: AutocompleteService<
+      AutocompleteSearchFilterable
+    >,
+    public patientPipe: PatientPipe
+  ) {}
+
+  /**
+   * on changes
+   * @param changes Ch-ch-ch-ch-changes
+   *
+   */
+  ngOnChanges(changes: SimpleChanges): void {
+    // tslint:disable-next-line: forin no-for-in
+    for (const propName in changes) {
+      const chng: SimpleChange = changes[propName];
+      const cur: string = JSON.stringify(chng.currentValue);
+      const prev: string = JSON.stringify(chng.previousValue);
+      console.log(`propName: currentValue = cur, previousValue = prev`);
+      console.log(
+        `chng cur prev ${chng.toString()} ${cur} ${prev}propName: currentValue = cur, previousValue = prev`
+      );
+    }
+
+    for (const propName of Object.keys(changes)) {
+      const chng: SimpleChange = changes[propName];
+      const cur: string = JSON.stringify(chng.currentValue);
+      const prev: string = JSON.stringify(chng.previousValue);
+      console.log(`propName: currentValue = cur, previousValue = prev`);
+      console.log(
+        `chng cur prev ${chng.toString()} ${cur} ${prev}propName: currentValue = cur, previousValue = prev`
+      );
+    }
+  }
   /**
    * on init
    */
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    console.log(
+      "AutocompletePatientComponent -> ngOnInit -> form control",
+      this.nestedPatientFormControl
+    );
 
+    const options: Observable<Patient[]> = this.autocompletePatientService.getSetupCreatedSingletonFilteredOptions(
+      this.passFormControl
+    );
+  }
 }
